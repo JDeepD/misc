7a5f6cf5d7a284e263794c2211fab09531ea26a4
Jaydeep P Das <jaydeepjd.8914@gmail.com>
2022-03-18 23:30:53 +0530

    assignment 8 done

--- assignment8/8.c
+++ assignment8/8.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define IN  1
+#define OUT 0
+
 int main(void)
 {
 	char filename[20], c;
-	int words, state, chars;
+	int words, chars, state;
+	words = chars = 0;
+	printf("Enter filename: ");
+	scanf("%s", filename);
 
+	FILE *fptr = fopen(filename, "r");
+
+	state = OUT;
+	while ((c = fgetc(fptr)) != EOF) {
+		chars++;
+		if (c == ' ' || c == '\n' || c == '\t') {
+			state = OUT;
+		}
+
+		else if (state == OUT) {
+			state = IN;
+			++words;
+		}
+	}
+
+	printf("%d words and %d chars in %s", words, chars, filename);
+	fclose(fptr);
+	
 	return 0;
 }
